d8a24e3e09caa0bed4f76c1ebab8dc90c1641afc
2023JoshuaP <jphilco@unsa.edu.pe>
2024-12-10 13:41:00 -0500

    Se usa variables globales para evitar duplicados. Fix #12.

--- src/main/java/com/social/controladores/UsersController.java
+++ src/main/java/com/social/controladores/UsersController.java
@@ -1,18 +1,11 @@
 package com.social.controladores;
 
-import java.util.LinkedList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import com.social.entidades.Usuario;
 import com.social.servicios.UsuarioService;
@@ -28,53 +21,56 @@ import com.social.servicios.UsuarioService;
 @Controller
 public class UsersController 
 {
+	private static final String USUARIO_ACTIVO = "usuarioActivo";
+	private static final String USER_LIST = "userList";
+
 	@Autowired 
 	private UsuarioService usersService;
 	
 	@RequestMapping("/users/lista-usuarios")
 	public String getList(Model model, Pageable pageable, @RequestParam(value = "", required=false) String searchText)
 	{	
-		Page<Usuario> usuarios = new PageImpl<Usuario>(new LinkedList<Usuario>());
+		Page<Usuario> usuarios;
 		Usuario usuarioActivo = usersService.getUsuarioActivo();
 		
-		if (searchText != null && !searchText.isEmpty()) 
-		{
-			usuarios = usersService
-				.buscarUsuariosPorNombreOEmail(pageable, searchText);
-			
-		} else 
-		{
+		if (searchText != null && !searchText.isEmpty()) {
+			usuarios = usersService.buscarUsuariosPorNombreOEmail(pageable, searchText);
+		}
+		else {
 			usuarios = usersService.getUsuarios(pageable);
 		}
-		model.addAttribute("usuarioActivo", usuarioActivo);
-		model.addAttribute("userList", usuarios.getContent());
+		model.addAttribute(USUARIO_ACTIVO, usuarioActivo);
+		model.addAttribute(USER_LIST, usuarios.getContent());
 		model.addAttribute("usuariosPeticionesEnviadas", usersService.getPeticionesEnviadas( usuarioActivo ));
 		model.addAttribute("page", usuarios);
 		return "/users/lista-usuarios";
-		
 	}
 	
 	@RequestMapping("/users/perfil/{username}")
 	public String getPerfil(Model model,@PathVariable String username)
 	{	
 		model.addAttribute("usuario", usersService.getUserByUsername(username));
-		model.addAttribute("usuarioActivo", usersService.getUsuarioActivo());
+		model.addAttribute(USUARIO_ACTIVO, usersService.getUsuarioActivo());
 		return "/users/perfil";
 		
 	}
 	
-	@RequestMapping(value="/user/edit", method=RequestMethod.POST)
+	@PostMapping(value="/user/edit")
 	public String editUser(Model model,  @ModelAttribute Usuario form)
 	{	
 		Usuario activo = usersService.getUsuarioActivo();
-		if(form.getNombre() != "")activo.setNombre(form.getNombre());
-		if(form.getApellidos() != "")activo.setApellidos(form.getApellidos());
-		if(form.getEmail() != "")activo.setEmail(form.getEmail());
-		if(form.getDescripcion() != "")activo.setDescripcion(form.getDescripcion());
+		if(form.getNombre() != null && !form.getNombre().isEmpty())
+			activo.setNombre(form.getNombre());
+		if(form.getApellidos() != null && !form.getApellidos().isEmpty())
+			activo.setApellidos(form.getApellidos());
+		if(form.getEmail() != null && !form.getEmail().isEmpty())
+			activo.setEmail(form.getEmail());
+		if(form.getDescripcion() != null && !form.getDescripcion().isEmpty())
+			activo.setDescripcion(form.getDescripcion());
 		
 		usersService.addUsuario(activo);
 		model.addAttribute("usuario", activo);
-		model.addAttribute("usuarioActivo", activo);
+		model.addAttribute(USUARIO_ACTIVO, activo);
 		return "/users/perfil";
 		
 	}
@@ -82,20 +78,18 @@ public class UsersController
 	@RequestMapping("/users/lista-amigos")
 	public String getAmigos(Model model, Pageable pageable, @RequestParam(value = "", required=false) String searchText)
 	{	
-		Page<Usuario> usuarios = new PageImpl<Usuario>(new LinkedList<Usuario>());
+		Page<Usuario> usuarios;
 		Usuario usuarioActivo = usersService.getUsuarioActivo();
 		
-		if (searchText != null && !searchText.isEmpty()) 
-		{
-			usuarios = usersService
-				.buscarUsuariosAmigosPorNombreOEmail(pageable, usuarioActivo, searchText);
+		if (searchText != null && !searchText.isEmpty()) {
+			usuarios = usersService.buscarUsuariosAmigosPorNombreOEmail(pageable, usuarioActivo, searchText);
 			
-		} else 
-		{
+		}
+		else {
 			usuarios = usersService.getUsuariosAmigos(pageable,usersService.getUsuarioActivo());
 		}
-		model.addAttribute("usuarioActivo", usersService.getUsuarioActivo());
-		model.addAttribute("userList", usuarios.getContent());
+		model.addAttribute(USUARIO_ACTIVO, usersService.getUsuarioActivo());
+		model.addAttribute(USER_LIST, usuarios.getContent());
 		model.addAttribute("page", usuarios);
 		return "/users/lista-amigos";
 		
@@ -109,12 +103,11 @@ public class UsersController
 		Usuario u2 = usersService.getUsuario( id );
 		usersService.addPeticionAmistad(u1, u2);
 		
-		Page<Usuario> usuarios = new PageImpl<Usuario>(new LinkedList<Usuario>());
-		usuarios = usersService
-				.buscarUsuariosPorNombreOEmail(pageable, searchText);
+		Page<Usuario> usuarios;
+		usuarios = usersService.buscarUsuariosPorNombreOEmail(pageable, searchText);
 		
-		model.addAttribute("usuarioActivo", u1);
-		model.addAttribute("userList", usuarios.getContent());
+		model.addAttribute(USUARIO_ACTIVO, u1);
+		model.addAttribute(USER_LIST, usuarios.getContent());
 		model.addAttribute("page", usuarios);
 		return "redirect:/users/lista-usuarios";
 	}
@@ -143,16 +136,15 @@ public class UsersController
 	@RequestMapping("/users/lista-peticiones")
 	public String getPeticiones(Model model, Pageable pageable)
 	{	
-		Page<Usuario> usuarios = new PageImpl<Usuario>(new LinkedList<Usuario>());
+		Page<Usuario> usuarios;
 		
 		Usuario usuarioActivo = usersService.getUsuarioActivo();
 		
 		usuarios = usersService.buscarPeticionesAmistad( pageable, usuarioActivo );
 		
-		model.addAttribute("usuarioActivo", usersService.getUsuarioActivo());
-		model.addAttribute("userList", usuarios.getContent());
+		model.addAttribute(USUARIO_ACTIVO, usersService.getUsuarioActivo());
+		model.addAttribute(USER_LIST, usuarios.getContent());
 		model.addAttribute("page", usuarios);
 		return "/users/peticiones-amistad";
 	}
-	
 }
