910fe923fa8ea7cd649e1b6c75984340f6c7f0c2
unknown <JoseAle123>
2024-12-12 13:06:49 -0500

    Corrigiendo los nombres de las variables #3

--- src/main/java/com/social/repositorios/PeticionAmistadRepository.java
+++ src/main/java/com/social/repositorios/PeticionAmistadRepository.java
@@ -25,12 +25,12 @@ public interface PeticionAmistadRepository extends CrudRepository<Amistad, Long>
 	@Transactional
 	@Modifying
 	@Query("DELETE FROM Amistad a WHERE a.usuario1 = ?1 AND a.usuario2= ?2") 
-	void delete(long id_u1, long id_u2);
+	void delete(long idU1, long idU2);
 	
 	@Query("SELECT u FROM Usuario u WHERE u.id IN( "
 			+ "SELECT a.usuario1 FROM Amistad a WHERE a.usuario2 = ?1)")
-	Page<Usuario> findAllByUsuario2(Pageable pageable, long id_u2);
+	Page<Usuario> findAllByUsuario2(Pageable pageable, long idU2);
 	
 	@Query("SELECT a FROM Amistad a WHERE a.usuario1 = ?1 AND a.usuario2 = ?2")
-	List<Amistad> findPeticiones(long id_u1, long id_u2);
+	List<Amistad> findPeticiones(long idU1, long idU2);
 }
